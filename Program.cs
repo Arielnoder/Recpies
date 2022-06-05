fadf445a36ba8839f0264ef292d180e917bcf860
tomerb12 <tomerorly@gmail.com>
2022-06-05 20:33:34 +0300

    hello

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+
 using Recipes.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +14,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddCoreAdmin();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
